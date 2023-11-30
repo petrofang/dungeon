d09dd2d0e9db1ca86071212bac5e9bc3da7a96c5
Giles Cooper <giles@cooper.gmail.com>
2023-11-30 09:27:28 -0500

    integrate command parser

--- dungeon.py
+++ dungeon.py
@@ -1,40 +1,65 @@
-from time import  sleep
-from mobiles import Monster, Mobile
+
+from mobiles import global_mobiles, Mobile
+from objects import Armor, Weapon, global_objects
+from rooms import global_rooms
 import players
 
 DEBUG=True
-def debug(message): print(f'{__name__} DEBUG:{message}') if DEBUG else None
+INFO=False
+PROMPT='\n > '
+def debug(message): print(f'{__name__} DEBUG: {message}') if DEBUG else None
+def info(message): print(f'INFO: {message}') if INFO else None
 debug(f'{DEBUG}')
-
-def who(Type:type=players.Mobile)-> dict:
-    ''' prints all active mobiles by name and object. '''
-    try:  
-        from gc import get_objects
-        mobileDictionary={[mob.name for mob in get_objects() if isinstance(mob, Type)][i]:[mob for mob in get_objects() if isinstance(mob, Type)][i] for i in range(len([mob for mob in get_objects() if isinstance(mob, Type)]))}
-        for i in mobileDictionary.keys(): print(f'{mobileDictionary[i].name:<20} :: {repr(mobileDictionary[i])}')
-    except AttributeError:
-        debug(f'{AttributeError}')
-        objList=[obj for obj in get_objects() if isinstance(obj, Type)]
-        print(objList)
+info(f'{INFO}')
 
 def main():
-    player=players.load()
-    if not player:player=players.new()
+    from commands import CommandList
+    while True:
+        user_input = input(PROMPT).lower()
+        command, *args = user_input.split()
+        command = getattr(CommandList, command, None)
+        if command: 
+            command(*args) if args else command()
+        else: print(f'unknown command "{user_input}"')
 
-    monster_horde=[]
-    monster_horde.append(Monster('goblin'))
-    monster_horde.append(Monster('ugly goblin'))
-    monster_horde.append(Monster('giant rat'))
-    monster_horde.append(Monster('skeleton', 20, 5))
-  #  monster_horde.append(Monster('vampire', 40, 10, 4))
-   # monster_horde.append(Monster('dragon', 200, 20, 5))
+if __name__=='__main__': 
+    # prepare rooms
+    from rooms import Room
+    workshop=Room("Antron's Workshop", "A cottage contains a small but messy workshop of various projects in varying states of incompletion.")
+    outside=Room("Potter's Field", 'The "Potter\'s field" is a place where potters have dug for clay, and thus a place conveniently full of trenches and holes for the burial of strangers.')
+    workshop.exits['south']     = outside
+    outside.exits['north']      = workshop
 
-    print([str(monster) for monster in monster_horde])
-    input('are you ready to fight the monster horde? > ')
-    print('rhetorical question... we begin now:')
-    sleep(1)
+    # load or generate a character:
+    user_input=input('(L)oad a character or (N)ew?' + PROMPT)
+    if user_input.upper() == 'L': me = players.load()
+    elif user_input.upper() == 'N': me = players.new()
+    else: raise UserWarning('Listen: if you cannot follow basic prompts, this game is not for you.')
+    if not me.room: me.room=workshop
 
-    player.fight(monster_horde)
-    player.save()
+    # prepare mobiles
+    Mobile('goblin', room=workshop)
+    Mobile('ugly goblin', room=workshop)
+    Mobile('giant rat', room=workshop)
+    Mobile('sickly kobold',5)
+    Mobile('skeleton', 20, 5)
+    Mobile('red goblin',23,4,1,room=workshop)
+    Mobile('gelatinous cube',40,5,5)
+    Mobile('vampire', 40, 10, 4)
+    Mobile('mimic',50,10,0)
+    Mobile('wraith',60,8,0, room=outside)
+    Mobile('dragonkin', 80,10,10) 
+    Mobile('dragon', 200, 20, 5)
 
-if __name__=='__main__': main()
+    # prepare objects
+    from objects import Weapon, Armor
+    sword=Weapon('a rusty old sword')
+    armor=Armor('some rusty old armor')
+    workshop.objects[sword.id] = sword
+    workshop.objects[armor.id] = armor
+    workshop.look()
+    outside.look()
+
+    # anything else before entering main game loop
+    
+    main()
