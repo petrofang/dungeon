9f7f3448a89098842fbfa8919eade1a2a80bf838
Giles Cooper <giles@cooper.gmail.com>
2023-12-02 00:52:36 -0500

    add Corpse subclass to Object.

--- objects.py
+++ objects.py
@@ -7,16 +7,13 @@ def info(message):  print(f'{__name__} INFO: {message}')  if INFO  else None
 debug(f'{DEBUG}')
 info(f'{INFO}')
 
-global_objects = {}
-
 class Object: 
-    def __init__(self, name:str='thing', **kwargs):
+    def __init__(self, name:str='item', **kwargs):
         from mobiles import Mobile
         from rooms import Room
         self.id = id(self)
         self.name = name
         info(f'{self.name} is created (ID:{self.id})')
-        if not isinstance(self, (Mobile, Room)): global_objects[self.id]=self
         for key, val in kwargs: self[key]=val
 
     def __del__(self):
@@ -32,5 +29,11 @@ class Weapon(Object):
 class Armor(Object):
     def __init__(self, name='armor', armor_rating:int=1):
         super().__init__(name)
-        self.rating = armor_rating
-    
+        self.rating = armor_rating   
+
+class Corpse(Object):
+    from rooms import Room
+    def __init__(self, name:str='corpse', room:Room=None):
+        super().__init__(name)
+        self.name = f"{name}'s corpse" if self.name else "corpse"
+        room.objects[self.id] = self
