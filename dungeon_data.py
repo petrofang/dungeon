8556ae3330bd00ede29901c928c924b8b5cbceb5
Dungeon <giles.cooper@gmail.com>
2024-02-21 14:52:35 -0500

    DATABASE constant for portability

--- dungeon_data.py
+++ dungeon_data.py
@@ -1,8 +1,10 @@
 #dungeon_data.py
 
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, JSON
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship
-engine = create_engine('mysql+mariadbconnector://dm:dungeonmaster@localhost/dungeon')
+DATABASE = 'mysql+mariadbconnector://dm:dungeonmaster@localhost/dungeon'
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, JSON, Boolean, and_
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship, joinedload
+engine = create_engine(DATABASE)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()  # Establish base class for models
