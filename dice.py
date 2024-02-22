d3e206f81fc0b52c8521a3c01b7a65751e5f423d
Dungeon <giles.cooper@gmail.com>
2024-02-22 15:31:48 -0500

    create dice roller module

---
+++ dice.py
@@ -0,0 +1,22 @@
+from random import random
+
+def d(n):
+    return int(1+(random()*n//1))
+
+def d6():
+    return int(1+(random()*6//1))
+
+def d8():
+    return int(1+(random()*8//1))
+
+def d10():
+    return int(1+(random()*10//1))
+
+def d12():
+    return int(1+(random()*12//1))
+
+def d20():
+    return int(1+(random()*20//1))
+
+def d100():
+    return int(1+(random()*100//1))
