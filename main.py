d44d574578ad6aab7232c10743a3f7625e1cc5f4
Dungeon <giles.cooper@gmail.com>
2024-02-22 18:10:23 -0500

    move command parsing functionality to commands.py

--- main.py
+++ main.py
@@ -12,30 +12,21 @@ def splash_screen():
 DEBUG=True
 PROMPT=' >> '
 
-import players, rooms
-from commands import CommandList
+import players, rooms, commands
 
 def debug(message): print(f'{__name__} *** DEBUG *** {message}') if DEBUG else None
 debug(f'{DEBUG}')
 
 def DEBUG_ROUTINE():
     me=players.load("test")
-    CommandList.fight("Rat", me=me)
     main(me)
     
 def main(me=players.PlayerCharacter):
-    rooms.look(me.room_id)
+    rooms.look(me)
     print(f'Hint: type HELP for a list of commands')
     while True:
-        user_input = input(PROMPT)
-        if user_input:
-            verb, *args = user_input.split()    
-            arg = " ".join(args)
-            command = getattr(CommandList, verb, None)
-            if command: 
-                command(arg, me=me) if arg else command(me=me)
-            else: print(f'Unknown command "{verb}".')
-        else: print('Huh?')
+        commands.parse(me, input(PROMPT))
+ 
 
 def init(): # initilization tasks:
     splash_screen()
@@ -45,7 +36,7 @@ def init(): # initilization tasks:
         user_input=input('(L)oad a character or (N)ew?' + PROMPT)
         if user_input.upper() == 'N': me = players.new()
         elif user_input.upper() == 'L': me = players.load()
-        else: print("...")
+        else: print("...")  
     main(me)
 
 if __name__=='__main__': 
