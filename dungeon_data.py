c560cfffd17c27cab72603f61fb0f64e5ce98a62
Dungeon <giles.cooper@gmail.com>
2024-02-22 15:32:27 -0500

    insert debug infrastructure and SQL echo

--- dungeon_data.py
+++ dungeon_data.py
@@ -1,10 +1,16 @@
 #dungeon_data.py
 
 DATABASE = 'mysql+mariadbconnector://dm:dungeonmaster@localhost/dungeon'
+DEBUG = False
 
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, JSON, Boolean, and_
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, MetaData, JSON, Boolean, and_, update
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship, joinedload
-engine = create_engine(DATABASE)
+if DEBUG:
+    engine = create_engine(DATABASE, echo=True)   
+else:
+    engine = create_engine(DATABASE, echo=False)
+
+
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()  # Establish base class for models
