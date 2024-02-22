07f81f2b3f96805f9514bbbdae4772d41b40eedf
Dungeon <giles.cooper@gmail.com>
2024-02-22 15:28:57 -0500

    strip out depreciated functionality in preparation for update

--- combat.py
+++ combat.py
@@ -2,30 +2,18 @@
 from random import random
 from time import sleep
 from mobiles import Mobile
+from dice import d
 
-DEBUG=False
-INFO=False
-def debug(message): print(f'{__name__} DEBUG: {message}') if DEBUG else None
-def info(message):  print(f'{__name__} INFO: {message}')  if INFO  else None
+DEBUG=True
+def debug(message): print(f'{__name__} *** DEBUG *** {message}') if DEBUG else None
 debug(f'{DEBUG}')
-info(f'{INFO}')
-
-def d20_roll(n:int=1): return n*(int(1+(random()*20//1)))
 
 def attack(me:Mobile, them:Mobile):
-    # TODO: attack as a mobile object method.
-        if them.dead:
-        #    debug(f'*** {{them}} {them} is dead... should not be attacking them')
-        #    raise RuntimeError(them) # they shouldn't get this far? or do we just skip?
-            pass
-
-        elif me.dead:
-            debug(f'*** {{me}} {me} is dead... me should not be attacking.')
-            raise RuntimeError(me) # this definitely shouldn't be possible   
-          
+        if them.hp <=0:
+            pass  
         else:
-            me.roll=d20_roll()
-            them.roll=d20_roll()
+            me.roll=d(20)
+            them.roll=d(20)
             print(f'{me} makes an attack at {them}!')
             sleep(2)
             print(f'{me}\'s attack: {me.attack}')
@@ -49,48 +37,19 @@ def attack(me:Mobile, them:Mobile):
                     if them.armor: 
                         print(f'{them.name.capitalize()}\'s {them.armor} prevents {them.armor.rating} damage')
                     print(f'{damage} damage done to {them}!')
-                    them.hit_points-=damage
+                    them.hp-=damage
             else:
                 print("Swing and a miss!")
             
             if killed(them): 
-                from players_OLD import PlayerCharacter
+                from players import PlayerCharacter
                 if isinstance(me, PlayerCharacter):
-                    me.level_up() 
-            else: fight(them, me)
-    
-def fight(me, them) -> Mobile:
-    ''' intiate a fight against a Mobile or list of Mobiles '''  
-    #TODO: handle grouping better... 
-    #IDEA: a "party" iterative object representing a group of Mobiles
-
-    if isinstance(me, list):  # PARTY COMBAT NOT IMPLEMENTED
-        raise NotImplementedError(me)
-        return     
-        for mobile in me:
-            if len(me)==1: 
-                debug(f'CONDENSING LIST: {me} only has 1 member left: {mobile}')
-                me=mobile
-            if isinstance(mobile, (Mobile)):
-                fight(mobile, them) 
-            else: 
-                debug(f'{mobile} is not a {Mobile}')
-                raise TypeError(mobile)
-            
-    if isinstance(them, list):
-        for each_mobile in them:
-            if isinstance(each_mobile, Mobile):
-                fight(me, each_mobile)
-            
-    elif isinstance(me, Mobile) and isinstance(them, Mobile):
-        attack(me, them)
+                    me.experience += 1
+                    return
+            else: attack(them, me)
 
 def killed(them:Mobile) -> bool: 
-    ''' them.die() if sub-zero hp. returns True|False for them.dead'''
-    if them.dead:
-        return True
-    if them.hit_points <= 0:
-        them.die()
+    if them.hp <= 0:
         return True
     else: 
         return False
