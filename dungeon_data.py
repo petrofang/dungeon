25c98aab4bcf78c4beb6d8b0c398d53a3d1b63ce
Dungeon <giles.cooper@gmail.com>
2024-02-23 00:21:24 -0500

    add metadata call (not sure what this even does)

--- dungeon_data.py
+++ dungeon_data.py
@@ -4,7 +4,7 @@ DATABASE = 'mysql+mariadbconnector://dm:dungeonmaster@localhost/dungeon'
 DEBUG = False
 
 # These imports are accessed by other modules:
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, MetaData, JSON, Boolean, and_, update
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, MetaData, JSON, Boolean, and_, update, delete
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship, joinedload
 
 if DEBUG:
@@ -12,7 +12,7 @@ if DEBUG:
 else:
     engine = create_engine(DATABASE, echo=False)
 
-
+metadata = MetaData()
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()  # Establish base class for models
