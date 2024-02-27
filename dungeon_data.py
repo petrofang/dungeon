8be918e4e7248c9e600262c91206721b233025d8
Dungeon <giles.cooper@gmail.com>
2024-02-26 21:27:38 -0500

    moved DATABASE declaration to main.py

--- dungeon_data.py
+++ dungeon_data.py
@@ -1,11 +1,10 @@
-#dungeon_data.py
 
-DATABASE = 'mysql+mariadbconnector://dm:dungeonmaster@localhost/dungeon'
 DEBUG = False
 
 # These imports are accessed by other modules:
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, MetaData, JSON, Boolean, and_, update, delete
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship, joinedload
+from main import DATABASE
 
 if DEBUG:
     engine = create_engine(DATABASE, echo=True)   
