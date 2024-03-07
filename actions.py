46840c02be0e69d35fd28257f7bd7a5a0dfb0f3e
Dungeon <giles.cooper@gmail.com>
2024-03-07 01:15:04 -0500

    change all local print statements to remote echo

--- actions.py
+++ actions.py
@@ -1,9 +1,7 @@
 import queue, time
-from main import PROMPT
 from objects import Object
-from mobiles import Mobile, MobilePrototype, MobileInventory, MobileEquipment
-from typing import Union
-from rooms import Exit, RoomInventory, cardinals
+from mobiles import Mobile, MobilePrototype
+from rooms import Exit, cardinals
 from dungeon_data import session
 
 action_queue=queue.Queue()
@@ -12,7 +10,7 @@ class Action():
     """
     This is the class for game engine actions. This is not to be 
     accessed directly. instead, use: 
-        do(self, action, arg, target)
+        do(subject, action, arg, target)
 
     commands.parse() and commands.CommandList handle player input,
     before sending SAAT (Subject, Action, Arguments, Target) to be
@@ -24,116 +22,162 @@ class Action():
     pass through this class.
 
     format:
-        def action(self:Mobile, arg:str, target, **kwargs):
+        def action(subject:Mobile, arg:str, target, **kwargs):
     """
     # TODO: better error and exception handling throughout
+    # TODO: consistant method parameters (positional, not keyword?)
+    #       (get rid of **kwargs)
+    # TODO: update all print() to Player.print() 
 
-    def say(self:Mobile, arg:str, target=None):
-        print(f'{self} says "{arg}"')
+    def say(subject:Mobile, arg:str, target=None):
+        echo_around(subject, f'{subject} says "{arg}"')
+        echo_at(subject, f'You say, "{arg}"')
     
-    def emote(self:Mobile, arg:str, target=None):
-        print(f"You {arg}.")
+    def emote(subject:Mobile, arg:str, target=None):
+        emote = arg
+        # TODO: echo at and around
+        echo(subject, f"{subject} {emote}.")
 
-    def spawn(self, arg, **kwargs):
+    def spawn(subject, arg, **kwargs):
         # arg = ID of the Mobile Prototype you'd like to spawn
         prototype=session.query(MobilePrototype).filter(
             MobilePrototype.id==arg).first()
-        prototype.spawn(invoker=self)
+        prototype.spawn(invoker=subject)
 
-    def echo(self:Mobile=None, arg:str=None, **kwargs):
-        print(f"{arg}")
-
-    def get(self:Mobile, target:Object, arg:Object=None, **kwargs):
-        # Remove the item from room and add to inventory
+    # TODO: check the positional argument here:
+    def get(subject:Mobile, target:Object, arg:Object=None, **kwargs):
+        # Remove the item from room or container and add to inventory
+        item, container = target, arg
         if arg and target:
             # if a container (arg) and item (target) are supplied
-            print(f"You get a {target} from the {arg}.")
-            arg.get(target)
-            self.add_to_inventory(target)
-        elif target:
-            print(f'You pick up {target}.')
-            self.room.remove(target)
-            self.add_to_inventory(target)
+            echo_at(subject, f"You get a {item} from the {container}.")
+            echo_around(subject, f"{subject} gets {item} from {container}.")
+            container.get(item)
+            subject.add_to_inventory(item)
+        elif item:
+            echo_at(subject, f'You pick up {item}.')
+            echo_around(subject, f'{subject} picks up {item}.')
+            subject.room.remove(item)
+            subject.add_to_inventory(item)
 
-    def put(self:Mobile, target:Object, arg:Object=None, **kwargs):
-        print(f"You put a {target} in the {arg}.")
+    def put(subject:Mobile, target:Object, arg:Object=None, **kwargs):
+        echo_at(subject, f"You put a {target} in the {arg}.")
+        echo_around(subject, f"{subject} puts {target} in the {arg}.")
         arg.put(target)
-        self.remove_from_inventory(target)
+        subject.remove_from_inventory(target)
 
-    def drop(self:Mobile, target:Object, **kwargs):
+    def drop(subject:Mobile, target:Object, **kwargs):
         # remove item from inventory and add to room
-        session.query(MobileInventory).filter(
-            MobileInventory.object_id == target.id
-            ).delete()
-        session.add(RoomInventory(
-            room_id=self.room.id, 
-            object_id=target.id))
+        subject.remove_from_inventory(target)
+        subject.room.add(target)
         session.commit()
-        print(f'You drop {target}.')
+        echo_at(subject, f'You drop {target}.')
+        echo_around(subject, f"{subject} drop {target}.")
 
-    def equip(self:Mobile, target:Object=None, **kwargs):
+    def equip(subject:Mobile, target:Object=None, **kwargs):
         # Add the item to Mobile_Equipment
         if target:
-            if self.equipment[target.type] == None:
-                if target in self.inventory:
-                    self.remove_from_inventory(target)
-                    self.equip(target)
-                    print(f"You equip {target.name}.")    
-            
-    def unequip(self:Mobile, target:Object=None, **kwargs):
+            if subject.equipment[target.type] == None:
+                if target in subject.inventory:
+                    subject.remove_from_inventory(target)
+                    subject.equip(target)
+                    echo_at(subject, f"You equip {target.name}.")    
+                    echo_around(subject, f"{subject} equips {target.name}.")
+
+    def unequip(subject:Mobile, target:Object=None, **kwargs):
         if target:
-            if target in self.equipment.values():
-                self.unequip(target)
-                self.add_to_inventory(target)
-                print(f"You unequip {target.name}.")
+            if target in subject.equipment.values():
+                subject.unequip(target)
+                subject.add_to_inventory(target)
+                echo_at(subject, f"You unequip {target.name}.")    
+                echo_around(subject, f"{subject} unequips {target.name}.")
 
-    def fight(self:Mobile, arg:str, target:Mobile):
-            print(f'{str(self).capitalize()} lunges at {target}...')
+    def fight(subject:Mobile, arg:str, target:Mobile):
+            echo_at(subject, f'You lunge at {target}...')
+            echo_around(subject, f'{subject} lunges at {target}...')
             from combat import Combat
-            Combat(self, target)
+            Combat(subject, target)
 
-    def go(self:Mobile, arg:str, **kwargs):
-        exit = self.room.exit(arg) 
+    def go(subject:Mobile, arg:str, **kwargs):
+        exit = subject.room.exit(arg) 
         if exit.is_open:
             if exit.direction in cardinals:
-                print(f'You go {exit.direction}...')
+                echo_at(subject, f'You go {exit.direction}...')
+                echo_around(subject, f'{subject} goes {exit.direction}.')
+                
             elif exit.entrance:
-                print(f'You enter the {exit.direction}.')
+                echo_at(subject, f'You enter the {exit.direction}...')
+                echo_around(subject, f'{subject} enters the {exit.direction}.')
             else:
-                print(f'You go through the {exit.direction}...')
+                echo_at(subject, f'You go through the {exit.direction}...')
+                echo_around(subject, f'{subject} goes through the {exit.direction}.')
             time.sleep(.5)
-            self.goto(exit.to_room.id)
-            self.room.look(self)
+            subject.goto(exit.to_room.id)
+            echo_around(subject, f"{subject} arrives from the {exit.backref.direction}")
+            subject.room.look(subject)
         else: 
             if exit.direction in cardinals:
-                print(f"The way {exit.direction} is closed.")
+                echo_at(subject, f"The way {exit.direction} is closed.")
             elif exit.entrance:
-                print(f"The entrance to the {exit.direction} is closed.")
+                echo_at(subject, f"The entrance to the {exit.direction} is closed.")
             else:
-                print(f"The {exit.direction} is closed.")
+                echo_at(subject, f"The {exit.direction} is closed.")
     
-    def open_door(self:Mobile, arg:str, target:Exit):
+    def open_door(subject:Mobile, arg:str, target:Exit):
         target.open()
 
-    def close_door(self:Mobile, arg:str, target:Exit):
+    def close_door(subject:Mobile, arg:str, target:Exit):
         target.close()
+    
 
-def do(self:Mobile = None, action:str = None, arg:str = None, target=None):
-    # check Action class for action:
+def do(subject:Mobile = None, action:str = None, arg:str = None, target=None):
+    # TODO: remove default arguments to these parameters
+    # TODO: but first, check all commands.CommandList methods
+    #           to make sure they call this function with positional arguments
     do_action=getattr(Action, action, None)
-    if do_action: do_action(self=self, arg=arg, target=target)
+    if do_action: do_action(subject=subject, arg=arg, target=target)
     else:
         print(f"* BUG *: action '{action}' not implemented.")
         raise NotImplementedError
 
-def add_to_queue(self=None, action=None, arg=None, target=None, **kwargs):   
-    action_queue.put((self, action, arg, target))
+def add_to_queue(subject=None, action=None, arg=None, target=None, **kwargs):  
+    """ add an action to the action queue to be performed at the next tick"""
+    action_queue.put((subject, action, arg, target))
 
 def update_game():
     if not action_queue.empty(): 
         print()
         while not action_queue.empty():
-            self, action, arg, target = action_queue.get(block=False) 
-            do(self, action, arg, target)
-        print(PROMPT)
-        
+            subject, action, arg, target = action_queue.get(block=False) 
+            do(subject, action, arg, target)
+
+def echo_at(player, message:str = "", end="\n"):
+    """
+    echo a message to player.
+    """
+    player.print(message, end)
+
+def echo_around(player, message:str = "", end="\n"):
+    """
+    echo a message to others in player's room.
+    """
+    if player.room.players:
+        for other in player.room.players:
+            if other.id != player.id:
+                echo_at(other, message, end)
+    
+def echo(player, message:str = "", end="\n"):
+    """
+    echo a message to all players in player's room.
+    """
+    if player.room.players:
+        for each in player.room.players:
+            echo_at(each, message, end)
+
+def echo_global(message:str = "", end="\n"):
+    """
+    echo a message to all players.
+    """
+    from players import connections
+    for one in connections:
+        echo_at(one, message, end)
