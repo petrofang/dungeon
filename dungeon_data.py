b70c39464a46efe37917aaa4d46580ccdcdcb1ff
Dungeon <giles.cooper@gmail.com>
2024-03-15 09:49:07 -0400

    migrate from MySQL to SQLite

--- dungeon_data.py
+++ dungeon_data.py
@@ -3,8 +3,10 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy import MetaData, JSON, Boolean, and_, update, delete
 # Some of these imports are accessed by other modules which import from here.
 
-DATABASE = 'mysql+mariadbconnector://dm:dungeonmaster@localhost/dungeon'
+# DATABASE = 'mysql+mariadbconnector://dm:dungeonmaster@localhost/dungeon'
+DATABASE = 'sqlite:///dungeon.db'
 # TODO : take this out of hardcode and put in config file with .gitignore
+# TODO : maybe use SQLite instead???
 
 VERBOSE = False
 
