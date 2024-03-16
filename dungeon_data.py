5ca0be8594d5c404875d77c82dac33ef134432b1
Dungeon <giles.cooper@gmail.com>
2024-03-15 21:53:56 -0400

    streamline verbose mode database queries

--- dungeon_data.py
+++ dungeon_data.py
@@ -3,17 +3,11 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy import MetaData, JSON, Boolean, and_, update, delete
 # Some of these imports are accessed by other modules which import from here.
 
-# DATABASE = 'mysql+mariadbconnector://dm:dungeonmaster@localhost/dungeon'
-DATABASE = 'sqlite:///dungeon.db'
-# TODO : take this out of hardcode and put in config file with .gitignore
-# TODO : maybe use SQLite instead???
-
 VERBOSE = False
+DATABASE = 'sqlite:///dungeon.db'
 
-if VERBOSE:
-    engine = create_engine(DATABASE, echo=True)   
-else:
-    engine = create_engine(DATABASE, echo=False)
+engine = create_engine(DATABASE, echo=True if VERBOSE else False) 
+
 
 metadata = MetaData()
 Session = sessionmaker(bind=engine)
